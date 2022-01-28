5fa8dbe208d38a39353a8dbcf4d685e2564c616a
Shivam Dhuria <shivam.dhuria07@gmail.com>
2022-01-29 00:56:27 +0530

    Initial commit

---
+++ app/src/main/java/com/theapache64/composeandroidtemplate/ui/screen/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.theapache64.composeandroidtemplate.ui.screen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.theapache64.composeandroidtemplate.ui.screen.dashboard.DashboardScreen
+import com.theapache64.composeandroidtemplate.ui.screen.splash.SplashScreen
+import com.theapache64.composeandroidtemplate.ui.theme.ComposeAndroidTemplateTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            ComposeAndroidTemplateTheme {
+                Surface {
+                    NavHost(navController = navController, startDestination = Screen.Splash.route) {
+
+                        // Splash
+                        composable(Screen.Splash.route) {
+                            SplashScreen(
+                                onSplashFinished = {
+                                    navController.popBackStack() // Remove splash from stack
+                                    navController.navigate(Screen.Dashboard.route) // Move to dashboard
+                                    println("Navigating...")
+                                }
+                            )
+                        }
+
+                        // Dashboard
+                        composable(Screen.Dashboard.route) {
+                            println("Recomposing...")
+                            DashboardScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
