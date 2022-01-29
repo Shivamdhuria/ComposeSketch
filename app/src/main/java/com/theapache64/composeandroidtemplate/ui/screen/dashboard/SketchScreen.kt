8c64ed65e0fe517b8aff9d01c9cb997e3cdb908d
Shivam Dhuria <abc@Shivams-MacBook-Pro.local>
2022-01-30 02:25:48 +0530

    added drawing engine to smooothen lines

--- app/src/main/java/com/theapache64/composeandroidtemplate/ui/screen/dashboard/SketchScreen.kt
+++ app/src/main/java/com/theapache64/composeandroidtemplate/ui/screen/dashboard/SketchScreen.kt
@@ -1,24 +1,32 @@
 package com.theapache64.composeandroidtemplate.ui.screen.dashboard
 
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.DraggableState
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.theapache64.composeandroidtemplate.util.DrawingEngine
 
 @Composable
 fun DashboardScreen(
     viewModel: SketchViewModel = hiltViewModel()
 ) {
+
+    val drawingEngine = DrawingEngine()
 
     Box(modifier = Modifier.fillMaxSize()) {
         Canvas(
@@ -39,17 +47,9 @@ fun DashboardScreen(
         ) {
             viewModel.lines.forEachIndexed { index, line ->
                 drawPath(
-                    path = Path().apply {
-                        line.points.forEachIndexed { i, point ->
-                            if (i == 0) {
-                                moveTo(point.x, point.y)
-                            } else {
-                                lineTo(point.x, point.y)
-                            }
-                        }
-                    },
+                    path = drawingEngine.createPath(line.points),
                     color = line.color,
-                    style = Stroke(line.strokeWidth)
+                    style = Stroke(line.strokeWidth, cap = StrokeCap.Round, join = StrokeJoin.Round)
                 )
             }
         }
@@ -63,5 +63,5 @@ fun DashboardScreen(
 @Composable
 fun CustomSlider(strokeWidth: Float, onChange: (Float) -> Unit) {
     Text(text = strokeWidth.toString())
-    Slider(value = strokeWidth, onValueChange = { onChange(it) }, valueRange = 1f..30f, steps =  0)
+    Slider(value = strokeWidth, onValueChange = { onChange(it) }, valueRange = 2f..30f, steps = 0)
 }
--- app/src/main/java/com/theapache64/composeandroidtemplate/util/DrawingEngine.kt
+++ app/src/main/java/com/theapache64/composeandroidtemplate/util/DrawingEngine.kt
@@ -1,12 +1,29 @@
 package com.theapache64.composeandroidtemplate.util
 
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Path
 
 class DrawingEngine {
 
-    fun calculateMidpoint(pointStart: Offset, pointEnd: Offset):Offset {
-        return Offset((pointStart.x + pointEnd.x) / 2, (pointStart.y + pointEnd.y) / 2)
+    fun createPath(points: List<Offset>): Path {
+        val path = Path()
+        points[0]?.let {
+            path.moveTo(it.x, it.y)
+        }
 
+        for (index in 1 until points.size) {
+            val midPoint = calculateMidpoint(points[index - 1], points[index])
+//            path.quadraticBezierTo(points[index].x, points[index].y, midPoint.x, midPoint.y)
+            path.quadraticBezierTo(points[index].x, points[index].y, midPoint.x, midPoint.y)
+        }
+
+        points[points.lastIndex]?.let {
+            path.lineTo(it.x, it.y)
+        }
+        return path
     }
 
+    fun calculateMidpoint(pointStart: Offset, pointEnd: Offset): Offset {
+        return Offset((pointStart.x + pointEnd.x) / 2, (pointStart.y + pointEnd.y) / 2)
+    }
 }
